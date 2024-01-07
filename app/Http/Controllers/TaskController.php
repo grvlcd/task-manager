c8085484e5e503a5763af280e30e20cb215d5a1e
Christian <generalpride@gmail.com>
2024-01-07 20:37:46 +0800

    add task and resources collection

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskCollection;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(Request $request) {
-        return response()->json(Task::all());
+    public function index(Request $request)
+    {
+        return new TaskCollection(Task::all());
     }
 }

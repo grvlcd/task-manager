6e6fada7818916d3c94f686566f5fd4336923554
Christian <generalpride@gmail.com>
2024-01-09 16:49:15 +0800

    restrict tasks when fetching projects

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -18,7 +18,7 @@ class ProjectResource extends JsonResource
             'id' => $this->id,
             'creator_id' => $this->creator_id,
             'title' => $this->title,
-            'tasks' => TaskResource::collection($this->tasks),
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
         ];
